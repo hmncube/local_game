f1fef968d97b62efad471550a8c6fdb80a8ed72a
happinessncube <happinessmncube@gmail.com>
2025-10-16 13:40:25 +0200

    New splash screen design

--- lib/presentation/splash/splash_screen.dart
+++ lib/presentation/splash/splash_screen.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:go_router/go_router.dart';
-import 'package:local_game/app/themes/app_text_styles.dart';
 import 'package:local_game/app/themes/app_theme.dart';
+import 'package:local_game/core/constants/app_assets.dart';
 import 'package:local_game/core/di/di.dart';
 import 'package:local_game/core/routes.dart';
 import 'package:local_game/presentation/splash/splash_cubit.dart';
@@ -28,12 +29,38 @@ class _SplashScreenState extends State<SplashScreen> {
         },
         child: Scaffold(
           backgroundColor: AppTheme.accentGreen,
-          body: Center(
-            child: Text(
-              'Learning Through Play,\nGrowing Through Games',
-              style: AppTextStyles.heading1.copyWith(color: Colors.white),
-              textAlign: TextAlign.center,
-            ),
+          body: Stack(
+            children: [
+              SizedBox(
+                width: double.infinity,
+                height: double.infinity,
+                child: SvgPicture.asset(
+                  fit: BoxFit.fill,
+                  AppAssets.backgroundSvg,
+                ),
+              ),
+              SvgPicture.asset(
+                  AppAssets.mavaraSvg,
+                ),
+              Center(
+                child: SvgPicture.asset(
+                  height: 400,
+                  width: 400,
+                    AppAssets.maskotSvg,
+                  ),
+              ),
+              Positioned(
+                bottom: 40,
+                left: 0,
+                right: 0,
+                child: 
+              SvgPicture.asset(
+                 height: 60,
+                width: double.infinity,
+                    AppAssets.progressBarSvg,fit: BoxFit.fill,
+                  ),
+              )
+            ],
           ),
         ),
       ),
@@ -41,3 +68,12 @@ class _SplashScreenState extends State<SplashScreen> {
   }
 }
 
+/*
+Center(
+            child: Text(
+              'Learning Through Play,\nGrowing Through Games',
+              style: AppTextStyles.heading1.copyWith(color: Colors.white),
+              textAlign: TextAlign.center,
+            ),
+          ),
+*/
