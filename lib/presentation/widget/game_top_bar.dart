2a2a063989d0382ef6e58df0c387403207a7791f
happinessncube <happinessmncube@gmail.com>
2025-11-06 15:50:35 +0200

    add back button

--- lib/presentation/widget/game_top_bar.dart
+++ lib/presentation/widget/game_top_bar.dart
@@ -1,9 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:local_game/app/themes/app_text_styles.dart';
 import 'package:local_game/app/themes/app_theme.dart';
 
 import 'package:flutter_svg/flutter_svg.dart';
 import 'package:local_game/core/constants/app_assets.dart';
+import 'package:local_game/core/routes.dart';
 
 class GameTopBar extends StatelessWidget {
   final int points;
@@ -29,60 +31,76 @@ class GameTopBar extends StatelessWidget {
             padding: const EdgeInsets.only(left: 16.0),
             child: Center(
               child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  const SizedBox(width: 8),
-                  SvgPicture.asset(height: 40, width: 40, AppAssets.coinSvg),
-                  const SizedBox(width: 4),
-                  Text(
-                    points.toString(),
-                    style: AppTextStyles.body.copyWith(
-                      color: AppTheme.accentGreen,
-                      fontSize: 24,
-                      fontWeight: FontWeight.bold,
+                  GestureDetector(
+                    onTap: () => context.go(Routes.mapScreen.toPath),
+                    child: SvgPicture.asset(
+                      height: 40,
+                      width: 40,
+                      AppAssets.homeSvg,
                     ),
                   ),
-                ],
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.center,
-            child: GestureDetector(
-              onTap:
-                  onHintClicked != null
-                      ? () {
-                        onHintClicked!();
-                      }
-                      : null,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  SvgPicture.asset(AppAssets.hintSvg, height: 30, width: 30),
+
+                  Row(
+                    children: [
+                      const SizedBox(width: 8),
+                      SvgPicture.asset(
+                        height: 30,
+                        width: 20,
+                        AppAssets.coinSvg,
+                      ),
+                      const SizedBox(width: 4),
+                      Text(
+                        points.toString(),
+                        style: AppTextStyles.body.copyWith(
+                          color: AppTheme.accentGreen,
+                          fontSize: 24,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    ],
+                  ),
+                  GestureDetector(
+                    onTap:
+                        onHintClicked != null
+                            ? () {
+                              onHintClicked!();
+                            }
+                            : null,
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        SvgPicture.asset(
+                          AppAssets.hintSvg,
+                          height: 30,
+                          width: 30,
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 14.0),
+                          child: Text(
+                            hints.toString(),
+                            style: AppTextStyles.body.copyWith(
+                              color: AppTheme.accentGreen,
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                   Padding(
-                    padding: const EdgeInsets.only(top: 14.0),
-                    child: Text(
-                      hints.toString(),
-                      style: AppTextStyles.body.copyWith(
-                        color: AppTheme.accentGreen,
-                        fontSize: 24,
-                        fontWeight: FontWeight.bold,
+                    padding: const EdgeInsets.only(right: 20.0),
+                    child: GestureDetector(
+                      child: SvgPicture.asset(
+                        AppAssets.settingsSvg,
+                        height: 30,
+                        width: 30,
                       ),
                     ),
                   ),
                 ],
-              ),
-            ),
-          ),
-          Align(
-            alignment: Alignment.centerRight,
-            child: Padding(
-              padding: const EdgeInsets.only(right: 20.0),
-              child: GestureDetector(
-                child: SvgPicture.asset(
-                  AppAssets.settingsSvg,
-                  height: 30,
-                  width: 30,
-                ),
               ),
             ),
           ),
